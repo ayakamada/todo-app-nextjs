f0b0ddf88a31ac76aedb8ab0b65b7dc9114feff4
ayakamada <sdkamada@gmail.com>
2023-06-29 22:29:56 +0900

    inputを用意

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,48 +7,62 @@ import Input from "../components/Input";
 import TodoList from "../components/TodoList";
 // import Todo from "../types/todo.type";
 // import useTodos from "../components/hooks/useTodos";
-import { TodoProvider } from "@/context/TodoContext";
+import { useTodoContext } from "@/context/TodoContext";
 
 export default function Home() {
-  const [todo, setTodo] = useState({ title: "", isDone: false });
-  const handleInputChange = () => {};
+  // const [todo, setTodo] = useState({ title: "", isDone: false });
+  const { addTodo } = useTodoContext();
+
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleInputChange = (value) => {
+    setNewTodo({
+      ...newTodo,
+      value,
+    });
+  };
+
   const handleCheckboxChange = () => {};
-  const handleSubmit = () => {};
+  const handleSubmit = () => {
+    addTodo(newTodo.value);
+
+    // after submitting clear the input
+    setNewTodo({
+      ...newTodo,
+      value: "",
+    });
+  };
+
   return (
-    <TodoProvider>
-      <div className="min-h-screen flex flex-col items-center">
-        <Head>
-          <title>Todo</title>
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap"
-            rel="stylesheet"
+    <div className="min-h-screen flex flex-col items-center">
+      <Head>
+        <title>Todo</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <div className="flex-1 lg:w-2/3 xl:w-2/5 w-full px-7">
+        <div>
+          <p className="text-3xl lg:text-4xl text-white font-bold tracking-widest pt-10 sm:pt-16 lg:pt-24 lg:pt-20 pb-6 lg:pb-10">
+            TODO
+            {/* <Toggle /> */}
+          </p>
+
+          <Input
+            todo={newTodo}
+            rounded
+            onInputChange={handleInputChange}
+            onCheckboxChange={handleCheckboxChange}
+            onSubmit={handleSubmit}
           />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        </Head>
 
-        <div className="flex-1 lg:w-2/3 xl:w-2/5 w-full px-7">
-          <div>
-            <p className="text-3xl lg:text-4xl text-white font-bold tracking-widest pt-10 sm:pt-16 lg:pt-24 lg:pt-20 pb-6 lg:pb-10">
-              TODO
-              {/* <Toggle /> */}
-            </p>
-
-            <Input
-              todo={todo}
-              rounded
-              onInputChange={handleInputChange}
-              onCheckboxChange={handleCheckboxChange}
-              onSubmit={handleSubmit}
-            />
-
-            <TodoList />
-          </div>
+          <TodoList />
         </div>
-
-        <Footer />
       </div>
-    </TodoProvider>
+
+      <Footer />
+    </div>
   );
 }
