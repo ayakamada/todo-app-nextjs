688238f31abe2b65d96f03ceead4472ac0889dce
ayakamada <sdkamada@gmail.com>
2023-06-29 22:30:18 +0900

    use TodoProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import { TodoProvider, useTodoContext } from "@/context/TodoContext";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <TodoProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </TodoProvider>
   );
 }
