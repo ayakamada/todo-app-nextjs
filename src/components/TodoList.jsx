a2a2d86384de6f48918379fcfac44744b102bb40
ayakamada <sdkamada@gmail.com>
2023-06-29 22:30:38 +0900

    set key

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,12 @@
 import { useTodoContext } from "@/context/TodoContext";
 
 export default function TodoList() {
-  const { todos, setTodos } = useTodoContext();
+  const { todos } = useTodoContext();
   return (
     <div className="mt-5 sm:mt-7">
       <div className="rounded">
-        {todos.map((todo) => (
-          <div key={todo.id} className="rounded">
+        {todos.map((todo, i) => (
+          <div key={i} className="rounded">
             {todo.title}
           </div>
         ))}
