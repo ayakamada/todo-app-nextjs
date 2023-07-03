284ae8656cad291ce68b7fb2543136dcf992c169
ayakamada <sdkamada@gmail.com>
2023-07-03 22:12:56 +0900

    set recoil root

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 import { TodoProvider, useTodoContext } from "@/context/TodoContext";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }) {
   return (
     <TodoProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <RecoilRoot>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RecoilRoot>
     </TodoProvider>
   );
 }
