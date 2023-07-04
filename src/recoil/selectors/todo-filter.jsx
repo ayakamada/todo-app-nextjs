2d5c447ff948c2f3b952051d3d8a10fcc9532270
ayakamada <sdkamada@gmail.com>
2023-07-04 22:27:34 +0900

    state状態によってfilterさせる

--- src/recoil/selectors/todo-filter.jsx
+++ src/recoil/selectors/todo-filter.jsx
@@ -1,24 +1,24 @@
 import { selector } from "recoil";
 
 import todoState from "@/recoil/atoms/todo";
-// import todoFilterState from "../atoms/todo-filter";
+import todoFilterState from "@/recoil/atoms/todo-filter";
 
 const filteredTodoState = selector({
   key: "filteredTodoState",
   get: ({ get }) => {
-    // const filter = get(todoFilterState);
+    const filter = get(todoFilterState);
     const list = get(todoState);
 
-    return list;
-
-    // switch (filter) {
-    //   case ShowActive:
-    //     return list.filter((item) => !item.isDone);
-    //   case ShowCompleted:
-    //     return list.filter((item) => item.isDone);
-    //   default:
-    //     return list;
-    // }
+    switch (filter) {
+      case "ShowAll":
+        return list;
+      case "ShowActive":
+        return list.filter((item) => !item.isDone);
+      case "ShowCompleted":
+        return list.filter((item) => item.isDone);
+      default:
+        return list;
+    }
   },
 });
 
