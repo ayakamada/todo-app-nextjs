8ef88f3347a17ccbfcdc7e2bce0eba93afacfed0
ayakamada <sdkamada@gmail.com>
2023-07-04 18:39:12 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import { ReactElement, useState } from "react";
 
-import Footer from "../components/Footer";
-import Input from "../components/Input";
-import TodoList from "../components/TodoList";
+import Footer from "@/components/Footer";
+import Input from "@/components/Input";
+import TodoList from "@/components/TodoList";
+import TodoListFilters from "@/components/TodoListFilters";
 import useTodos from "@/hooks/useTodos";
-// import { useTodoContext } from "@/context/TodoContext";
 
 export default function Home() {
   const { addTodo } = useTodos();
@@ -45,7 +45,7 @@ export default function Home() {
 
       <div className="flex-1 lg:w-2/3 xl:w-2/5 w-full px-7">
         <div>
-          <p className="text-3xl lg:text-4xl text-white font-bold tracking-widest pt-10 sm:pt-16 lg:pt-24 lg:pt-20 pb-6 lg:pb-10">
+          <p className="text-3xl lg:text-4xl text-white font-bold tracking-widest pt-10 sm:pt-16 lg:pt-24 pb-6 lg:pb-10">
             TODO
             {/* <Toggle /> */}
           </p>
@@ -58,6 +58,7 @@ export default function Home() {
           />
 
           <TodoList />
+          <TodoListFilters />
         </div>
       </div>
 
