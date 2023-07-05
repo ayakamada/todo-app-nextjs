fe24d8db75191a4555a11a184e468d98e6ec4cb4
ayakamada <sdkamada@gmail.com>
2023-07-05 23:13:56 +0900

    react-sortable-hoc使用準備

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,26 +1,41 @@
+import React, { Fragment, useCallback } from "react";
+import {
+  SortableContainer as sortableContainer,
+  SortableElement as sortableElement,
+  SortEvent,
+  SortEventWithTag,
+} from "react-sortable-hoc";
+
 import useTodos from "@/hooks/useTodos";
 
 import CloseIcon from "@/svgs/closeIcon";
 import CheckedIcon from "@/svgs/checkedIcon";
 
-const TodoItem = ({ todo, idx, deleteTodo, updateTodoIsDone }) => {
+const SortableContainer = sortableContainer(({ children }) => {
+  return <ul>{children}</ul>;
+});
+
+const TodoItem = sortableElement(({ todo, idx, deleteTodo, updateTodoIsDone }) => {
   return (
-    <div className="flex justify-between items-center">
-      <div className="flex items-center">
-        <div
-          className={`w-6 h-6 flex items-center justify-center rounded-full border dark:border-white/50  mr-4 hover:border-indigo-500 ${
-            todo.isDone ? "bg-gradient-to-r from-cyan-500 to-purple-500 border-0" : ""
-          }`}
-          onClick={() => updateTodoIsDone(idx, !todo.isDone)}
-        >
-          <CheckedIcon className={` ${todo.isDone ? "text-white w-4 h-4 mx-auto" : "invisible"}`} />
-        </div>
-        <p className="text-sm sm:text-base">{todo.title}</p>
-      </div>
-      <div className="w-5 h-5 relative">
-        <a id={`delete`} className="absolute top-0 right-0" onClick={() => deleteTodo(idx)}>
-          <CloseIcon
-            className="
+    <li key={todo.id} className="list-none first:rounded-t last:rounded-b">
+      <div className="group transition ease-in-out cursor-pointer py-4 sm:py-4.5 pr-8 pl-8 bg-white dark:bg-white  border-b  border-gray-200">
+        <div className="flex justify-between items-center text-slate-800 ">
+          <div className="flex items-center">
+            <div
+              id="checkBtn"
+              className={`w-6 h-6 flex items-center justify-center rounded-full border dark:border-white/50  mr-4 hover:border-indigo-500 ${
+                todo.isDone ? "bg-gradient-to-r from-cyan-500 to-purple-500 border-0" : ""
+              }`}
+              onClick={() => updateTodoIsDone(idx, !todo.isDone)}
+            >
+              <CheckedIcon className={` ${todo.isDone ? "text-white w-4 h-4 mx-auto" : "invisible"}`} />
+            </div>
+            <p className="text-sm sm:text-base">{todo.title}</p>
+          </div>
+          <div className="w-5 h-5 relative">
+            <a id={`delete`} className="absolute top-0 right-0" onClick={() => deleteTodo(idx)}>
+              <CloseIcon
+                className="
               sm:w-6
               sm:h-6
               dark:text-white
@@ -31,27 +46,41 @@ const TodoItem = ({ todo, idx, deleteTodo, updateTodoIsDone }) => {
               opacity-0
               group-hover:opacity-100
             "
-          />
-        </a>
+              />
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
+    </li>
   );
-};
+});
 
 export default function TodoList() {
-  const { todos, deleteTodo, updateTodoIsDone } = useTodos();
+  const { todos, deleteTodo, updateTodoIsDone, reorderTodo } = useTodos();
+
+  const onSortEnd = useCallback(
+    ({ oldIndex, newIndex }) => {
+      reorderTodo(oldIndex, newIndex);
+    },
+    [reorderTodo]
+  );
+
+  const shouldCancelStart = useCallback((e) => {
+    // クリックした要素のIDが”checkBtn”場合はドラッグを開始しない
+    if (e.target.id === "checkBtn") {
+      return true;
+    }
+
+    return false;
+  }, []);
+
   return (
     <div className="mt-5 sm:mt-7">
-      <div className="rounded">
+      <SortableContainer onSortEnd={onSortEnd} shouldCancelStart={shouldCancelStart}>
         {todos.map((todo, i) => (
-          <div
-            key={i}
-            className="group transition ease-in-out cursor-pointer py-4 sm:py-4.5 pr-8 pl-8 dark:bg-gray-800 first:rounded-t last:rounded-b border-b border-gray-600"
-          >
-            <TodoItem todo={todo} deleteTodo={deleteTodo} updateTodoIsDone={updateTodoIsDone} idx={i} />
-          </div>
+          <TodoItem index={i} todo={todo} deleteTodo={deleteTodo} updateTodoIsDone={updateTodoIsDone} idx={i} />
         ))}
-      </div>
+      </SortableContainer>
     </div>
   );
 }
