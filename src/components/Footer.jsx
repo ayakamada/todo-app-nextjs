87388a011ef8cc9fc5d15351118a5b7a576650cb
ayakamada <sdkamada@gmail.com>
2023-07-13 15:23:33 +0900

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 export default function Footer() {
-  return(
+  return (
     <footer>
-      footer
+      <div className="text-sm text-center mt-10 py-3">
+        <a href="https://github.com/cloworm" target="_blank" rel="noopener noreferrer" className="group">
+          <div className="inline-block github align-top h-4 w-4 bg-light_darkGreyBlue group-hover:bg-black dark:group-hover:bg-white transition ease-linear"></div>
+          <span className="pl-1.5 text-light_darkGreyBlue dark:text-dark_veryDarkGreyBlue group-hover:text-black dark:group-hover:text-white transition ease-linear">
+            ayakamada
+          </span>
+        </a>
+      </div>
     </footer>
-  )
+  );
 }
